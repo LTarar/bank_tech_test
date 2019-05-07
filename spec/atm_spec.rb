1a53ecd8482a5e2e73ca31145757d23c1e2593c8
Wardah <wlt_93@hotmail.com>
2019-05-07 12:33:21 +0100

    Refactored tests to avoid repetition

--- spec/atm_spec.rb
+++ spec/atm_spec.rb
@@ -1,14 +1,14 @@
 require 'atm'
 
 describe ATM do
+  subject(:atm) { ATM.new }
+
   it 'checks for account balance to be zero by default' do
-    atm = ATM.new
     expect(atm.balance).to eq 0
   end
 
   describe "#deposit" do
     it 'checks for balance after depositing money into atm' do
-      atm = ATM.new
       atm.deposit(500)
       expect(atm.balance).to eq 500
     end
@@ -16,7 +16,6 @@ describe ATM do
 
   describe "#withdraw" do
     it 'checks for balance after withdrawing money from atm' do
-      atm = ATM.new
       atm.deposit(500)
       atm.withdraw(200)
       expect(atm.balance).to eq 300
