bfb0ead2c6cc05fb2bdf30e10eef54e35ca0876b
Wardah <wlt_93@hotmail.com>
2019-05-08 11:24:35 +0100

    Refactored. Extracted methods following SRP in code and test file

--- lib/atm.rb
+++ lib/atm.rb
@@ -1,28 +1,39 @@
 require 'date'
 
 class ATM
-  attr_reader :balance
+  attr_reader :current_balance
 
   def initialize
-    @balance = 0
+    @current_balance = 0
   end
 
   def deposit(money)
-    @balance_after_deposit = '%.2f' % (@balance += money)
-    @deposit_amount = '%.2f' % money
-    date = (Date.today).to_s
-    @date_of_deposit = Date.parse(date).strftime("%d/%m/%Y")
+    @date_of_deposit = date_format
+    @deposit_amount = currency_format(money)
+    @balance_after_deposit = currency_format(@current_balance += money)
   end
 
   def withdraw(money)
-    @balance_after_withrawal = '%.2f' % (@balance -= money)
-    @withrawal_amount = '%.2f' % (money)
-    date = (Date.today).to_s
-    @date_of_withdrawal = Date.parse(date).strftime("%d/%m/%Y")
+    @date_of_withdrawal = date_format
+    @withrawal_amount = currency_format(money)
+    @balance_after_withrawal = currency_format(@current_balance -= money)
   end
 
   def print_statement
-    "date || credit || debit || balance\n#{@date_of_withdrawal} || || #{@withrawal_amount} || #{@balance_after_withrawal}\n#{@date_of_deposit} || #{@deposit_amount} || || #{@balance_after_deposit}"
+    "date || credit || debit || balance
+    #{@date_of_withdrawal} || || #{@withrawal_amount} || #{@balance_after_withrawal}
+    #{@date_of_deposit} || #{@deposit_amount} || || #{@balance_after_deposit}"
+  end
+
+  private
+
+  def currency_format(number)
+    '%.2f' % (number)
+  end
+
+  def date_format
+    date = (Date.today).to_s
+    Date.parse(date).strftime("%d/%m/%Y")
   end
   
 end
--- spec/atm_spec.rb
+++ spec/atm_spec.rb
@@ -3,14 +3,19 @@ require 'atm'
 describe ATM do
   subject(:atm) { ATM.new }
 
+  let(:output) { "date || credit || debit || balance
+    08/05/2019 || || 200.00 || 300.00
+    08/05/2019 || 500.00 || || 500.00" 
+  }
+
   it 'checks for account balance to be zero by default' do
-    expect(atm.balance).to eq 0
+    expect(atm.current_balance).to eq 0
   end
 
   describe "#deposit" do
     it 'checks for balance after depositing money into atm' do
       atm.deposit(500)
-      expect(atm.balance).to eq 500
+      expect(atm.current_balance).to eq 500
     end
   end
 
@@ -18,16 +23,15 @@ describe ATM do
     it 'checks for balance after withdrawing money from atm' do
       atm.deposit(500)
       atm.withdraw(200)
-      expect(atm.balance).to eq 300
+      expect(atm.current_balance).to eq 300
     end
   end
 
   describe "#print_statement" do
     it 'checks for account statement being printed with date, amount, balance' do
-      atm = ATM.new
       atm.deposit(500)
       atm.withdraw(200)
-      expect(atm.print_statement).to eq "date || credit || debit || balance\n08/05/2019 || || 200.00 || 300.00\n08/05/2019 || 500.00 || || 500.00"
+      expect(atm.print_statement).to eq output
     end
   end
 
